f43d62cd239dbcc9a004020f167fd591b7eb6f93
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-27 06:12:11 +0600

    router setup done

---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftNav = () => {
+  return (
+    <div>
+      <h3>Left Nav</h3>
+    </div>
+  );
+};
+
+export default LeftNav;
---
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightNav = () => {
+  return (
+    <div>
+      <h3>Right Nav</h3>
+    </div>
+  );
+};
+
+export default RightNav;
