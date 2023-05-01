fc06e1c2d01eb2dbd07a2cfcace860f2d72d921e
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-02 02:00:55 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log("environment variable", import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyAqYcO0EBwWnhq0fbVhrtIpnEuqYSxaTS0",
-  authDomain: "the-news-dragon-c243a.firebaseapp.com",
-  projectId: "the-news-dragon-c243a",
-  storageBucket: "the-news-dragon-c243a.appspot.com",
-  messagingSenderId: "693709226672",
-  appId: "1:693709226672:web:d5a12453184ee86ec6b175",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
